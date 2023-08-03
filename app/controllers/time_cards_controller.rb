286c3ea2f6fc03db2c7304c1f692dbd4943e4b47
shuntaron <130245866+shuntaron@users.noreply.github.com>
2023-08-03 21:33:07 +0900

    Update time_cards#scrape feature

--- app/controllers/time_cards_controller.rb
+++ app/controllers/time_cards_controller.rb
@@ -2,6 +2,10 @@ class TimeCardsController < ApplicationController
   def index
     @time_cards = TimeCard.all
   end
+  
+  def scrape
+    redirect_to root_path, notice: "スクレイピングが成功しました"
+  end
 
   def edit
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: "time_cards#index"
-  resources :time_cards
+  resources :time_cards do
+    collection do
+      post :scrape
+    end
+  end
 end
