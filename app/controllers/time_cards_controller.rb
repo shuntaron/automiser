46eb0b7e12660a2dcd35d3fa6223a59e6ae83049
shuntaron <130245866+shuntaron@users.noreply.github.com>
2023-08-03 21:45:03 +0900

    Update time_cards#scrape feature

--- app/controllers/time_cards_controller.rb
+++ app/controllers/time_cards_controller.rb
@@ -4,6 +4,15 @@ class TimeCardsController < ApplicationController
   end
   
   def scrape
+    require "playwright"
+    Playwright.create(playwright_cli_executable_path: 'npx playwright') do |playwright|
+      playwright.chromium.launch(headless: false) do |browser|
+        page = browser.new_page
+        page.goto(ENV["SCRAPE_URL"])
+        page.wait_for_timeout(1000)
+        page.wait_for_selector("#time_cards")
+      end
+    end
     redirect_to root_path, notice: "スクレイピングが成功しました"
   end
 
